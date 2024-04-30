3e08f33ebace55ede08e7eca806097ebaf156443
Mwandingi, Kenneth <k.n.m.mwandingi@tue.nl>
2024-04-30 10:43:37 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='alliander-load-profile-estimation',
+    author='Kenneth, Gohar, Semawit, Yamon',
+    license='',
+)
